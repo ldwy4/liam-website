5835c9f0a4aed471fc76ea71d59419234cfca669
liam <liam.dwyer@vipre.com>
2021-08-09 16:41:43 -0700

    More changes to sheets page

--- src/components/sheets.jsx
+++ src/components/sheets.jsx
@@ -15,8 +15,13 @@ class Sheets extends Component {
       otherURL: "https://api.tbirdsync.com/create?" + "uid=" + Cookies.get('id'),
       loaded: false,
       loading: false,
+      creating: false,
+      created: false,
+      selected: false,
     };
-    this.showSheets = this.showSheets.bind(this)
+    this.showSheets = this.showSheets.bind(this);
+    this.createSheet = this.createSheet.bind(this);
+    this.selectSheet = this.selectSheet.bind(this);
   }
 
   showSheets() {
@@ -36,15 +41,28 @@ class Sheets extends Component {
             id: data[i][1],
           });
         }
-        // for (var run in data) {
-        //   console.log(run);
-        //   const key = Math.floor(Math.random() * 1000);
-
-        //   runs.push({ key: key, date: run[0], distance: run[1], time: run[2] });
-        // }
         this.setState({ sheets: sheets, loaded: true, loading: false, });
       })
     );
+  }
+
+  createSheet() {
+    this.setState({ creating: true });
+    const url = this.state.otherURL;
+    fetch(url, {
+      method: 'POST',
+      credentials: 'include'
+    })
+      .then(response => response.json().then(this.setState({ created: true, creating: false, })));
+  }
+
+  selectSheet() {
+    const url = this.state.URL;
+    fetch(url, {
+      method: 'POST',
+      credentials: 'include'
+    })
+      .then(response => response.json().then(this.setState({ selected: true })));
   }
 
   handleSubmit(e) {
@@ -90,43 +108,69 @@ class Sheets extends Component {
     console.log(this.state.UID)
     return (
       <section style={{ display: "flex", flexDirection: "row", height: "auto", minHeight: "100%", overflow: "auto" }}>
-        <div className="sheets">
+        <div className={this.state.creating | this.state.created ? "sheetsShrink" : "sheets"}>
+          {!this.state.selected ?
+            <div className="content">
+
+              <h3 className="sheets-title">Select an existing training log</h3>
+              {this.state.loading &&
+                <Loader
+                  type="TailSpin"
+                  color="#00BFFF"
+                  height={40}
+                  width={40}
+                  timeout={5000} //3 secs
+                />
+              }
+              {!this.state.loaded ?
+                <button className={this.state.loading ? "btn btn-danger mt-3" : "btn btn-danger"} onClick={this.showSheets}>Select</button>
+                :
+                <div className="list">
+                  {this.state.sheets.map((sheet) => (
+                    <Sheet
+                      key={sheet.key}
+                      title={sheet.title}
+                      id={sheet.id}
+                      onChangeId={this.handleChangeId(sheet.id)}
+                      active={this.state.id === sheet.id}
+                    />
+                  ))}
+                  <button className="btn btn-danger" onClick={this.selectSheet}>Submit</button>
+                </div>
+              }
+            </div>
+            :
+            <div className="content">
+              <h3 className="sheets-title">Training log saved!</h3>
+              <svg style={{ width: '40px' }} version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+                <circle className="path circle" fill="none" stroke="#73AF55" strokeWidth="6" strokeMiterlimit="10" cx="65.1" cy="65.1" r="62.1" />
+                <polyline className="path check" fill="none" stroke="#73AF55" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 " />
+              </svg>
+            </div>
+          }
+        </div>
+        {/* <div class="vl"></div> */}
+        <div className={!this.state.loaded && !this.state.loading ? "create" : "createShrink"}>
           <div className="content">
-            <h3 className="sheets-title">Select an existing training log</h3>
-            {this.state.loading &&
+            <h3 className="create-title">{this.state.created ? "Created new training log!" : "Create a new training log for this year"}</h3>
+            {this.state.creating &&
               <Loader
+                className="mb-3"
                 type="TailSpin"
                 color="#00BFFF"
                 height={40}
                 width={40}
-                timeout={5000} //3 secs
               />
             }
-            {!this.state.loaded ?
-              <button className={this.state.loading ? "btn btn-danger mt-3" : "btn btn-danger" } onClick={this.showSheets}>Select</button>
-              :
-              <form className="list" action={this.state.url} method="POST">
-                {this.state.sheets.map((sheet) => (
-                  <Sheet
-                    key={sheet.key}
-                    title={sheet.title}
-                    id={sheet.id}
-                    onChangeId={this.handleChangeId(sheet.id)}
-                    active={this.state.id === sheet.id}
-                  />
-                ))}
-                <button className="btn btn-danger" type="submit" value={this.state.id}>submit</button>
-              </form>
+            {!this.state.creating && !this.state.created &&
+              <button className="btn btn-primary" onClick={this.createSheet} >Create</button>
             }
-          </div>
-        </div>
-        {/* <div class="vl"></div> */}
-        <div className={!this.state.loaded ? "create" : "createShrink"}>
-          <div className="content">
-            <h3 className="create-title">Create a new training log for this year</h3>
-            <form action={this.state.otherURL} method="POST">
-              <button className="btn btn-primary" type="submit" value={this.state.UID}>Create</button>
-            </form>
+            {this.state.created &&
+              <svg style={{ width: '40px' }} version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+                <circle className="path circle" fill="none" stroke="#73AF55" strokeWidth="6" strokeMiterlimit="10" cx="65.1" cy="65.1" r="62.1" />
+                <polyline className="path check" fill="none" stroke="#73AF55" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 " />
+              </svg>
+            }
           </div>
         </div>
       </section>
